ae74d6d4e783d5817028c1a2cf3fcfe55fa848f3
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2020-01-20 20:31:35 -0600

    Changed Stuart bot to run server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python server.py
+web: python stuart-bot.py
--- stuart-bot.py
+++ stuart-bot.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from flask import Flask
 import datetime
 import requests
 import tweepy
@@ -6,13 +7,47 @@ import time
 import cv2
 import os
 
-load_dotenv()
-CAM_URL = os.environ("CAM_URL")
-CONSUMER_KEY = os.environ('CONSUMER_KEY')
-CONSUMER_SECRET = os.environ('CONSUMER_SECRET')
-ACCESS_KEY = os.environ('ACCESS_KEY')
-ACCESS_SECRET = os.environ('ACCESS_SECRET')
-searching_for_cat = True
+
+app = Flask(__name__)
+    
+@app.route('/')
+def home():
+    CAM_URL = os.environ("CAM_URL")
+    CONSUMER_KEY = os.environ('CONSUMER_KEY')
+    CONSUMER_SECRET = os.environ('CONSUMER_SECRET')
+    ACCESS_KEY = os.environ('ACCESS_KEY')
+    ACCESS_SECRET = os.environ('ACCESS_SECRET')
+    searching_for_cat = True
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    while True:
+        if searching_for_cat:
+            try:
+                take_snapshot("snapshot.jpg")
+                image = cv2.imread("snapshot.jpg")
+                rects = any_cats(image)
+
+                if len(rects) > 0:
+                    print('Cat Detected')
+                    api.update_with_media("snapshot.jpg")
+                    searching_for_cat = False
+                else:
+                    print('No cat detected')
+            
+            except requests.RequestException as e:
+                print(f"Webcam inaccessible.")
+                print(f"{e}")
+            except Exception as e:
+                print(f"{e}")
+            
+            time.sleep(5)
+        else:
+            time.sleep(60 * 50)
+            searching_for_cat = True
+
 
 def any_cats(image, classifier = "haarcascade_frontalcatface_extended.xml"):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -32,31 +67,5 @@ def take_snapshot(filename, omega_url = f"http://{CAM_URL}/?action=snapshot"):
     else:
         raise requests.RequestException()
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
-
-while True:
-    if searching_for_cat:
-        try:
-            take_snapshot("snapshot.jpg")
-            image = cv2.imread("snapshot.jpg")
-            rects = any_cats(image)
-
-            if len(rects) > 0:
-                print('Cat Detected')
-                api.update_with_media("snapshot.jpg")
-                searching_for_cat = False
-            else:
-                print('No cat detected')
-        
-        except requests.RequestException as e:
-            print(f"Webcam inaccessible.")
-            print(f"{e}")
-        except Exception as e:
-            print(f"{e}")
-        
-        time.sleep(5)
-    else:
-        time.sleep(60 * 50)
-        searching_for_cat = True
+if __name__ == '__main__':
+    app.run()
