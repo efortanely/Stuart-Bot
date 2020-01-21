a605cabc376c6aa2e5870684a45a5b2e561db8e1
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2020-01-20 20:48:04 -0600

    fixed syntax

--- stuart-bot.py
+++ stuart-bot.py
@@ -9,13 +9,14 @@ import os
 
 app = Flask(__name__)
     
+CAM_URL = os.environ("CAM_URL")
+CONSUMER_KEY = os.environ('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ('CONSUMER_SECRET')
+ACCESS_KEY = os.environ('ACCESS_KEY')
+ACCESS_SECRET = os.environ('ACCESS_SECRET')
+
 @app.route('/')
 def home():
-    CAM_URL = os.environ("CAM_URL")
-    CONSUMER_KEY = os.environ('CONSUMER_KEY')
-    CONSUMER_SECRET = os.environ('CONSUMER_SECRET')
-    ACCESS_KEY = os.environ('ACCESS_KEY')
-    ACCESS_SECRET = os.environ('ACCESS_SECRET')
     searching_for_cat = True
 
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
